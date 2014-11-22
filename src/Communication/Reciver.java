f8ba27081cdf09fa97a3178af0f50da413f490c1
tgm-patrick-koc <cash1v@hotmail.com>
2014-11-22 21:21:06 +0100

    Reciver updated

--- src/Communication/Reciver.java
+++ src/Communication/Reciver.java
@@ -1,19 +1,20 @@
 package Communication;
 
-import javax.jms.Connection;
-import javax.jms.Destination;
-import javax.jms.MessageConsumer;
-import javax.jms.Session;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
 
 public class Reciver implements Runnable {
 
 	private Session session=null;
 
-	private Connection connection=null;
+	private Connection connection;
 
-	private MessageConsumer producer=null;
+	private MessageConsumer consumer=null;
 
 	private Destination destination=null;
+
+	private ConnectionFactory connectionFactory;
 
 	private String user;
 
@@ -25,24 +26,72 @@ public class Reciver implements Runnable {
 
 	private boolean runState;
 
-	public Reciver(String user, String password, String url) {
-		this.user=user;
-	}
-
-	public void Reciver(String user, String password, String url, String chatroom) {
-
-	}
-
-	public void start() {
-
-	}
-
-	public void stop() {
-
-	}
+	private Message message;
 
 	@Override
 	public void run() {
+		try {
+			if(runState == true) {
+				message = (TextMessage) consumer.receive();
+			}
+		} catch (JMSException e) {
+
+		}
+	}
+
+	public Reciver(String user, String password, String url) {
+		this.user=user;
+		this.password=password;
+		this.url=url;
+
+		connectionFactory = new ActiveMQConnectionFactory(user, password, url);
+		try {
+			connection = connectionFactory.createConnection();
+			session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+			destination = session.createTopic( chatroom );
+			consumer = session.createConsumer( destination );
+
+		} catch (JMSException e) {
+
+		}
 
 	}
+
+	public void Reciver(String user, String password, String url, String chatroom) {
+		this.user=user;
+		this.password=password;
+		this.url=url;
+
+		connectionFactory = new ActiveMQConnectionFactory(user, password, url);
+		try {
+			connection = connectionFactory.createConnection();
+			session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+			destination = session.createTopic( chatroom );
+			consumer = session.createConsumer( destination );
+
+		} catch (JMSException e) {
+
+		}
+	}
+
+	public void start() {
+		try {
+			connection.start();
+		} catch (JMSException e) {
+
+		}
+	}
+
+	public void stop() {
+		runState=false;
+		try {
+			consumer.close();
+			session.close();
+			connection.close();
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
+
+	}
+
 }
---
+++ src/bsp/JMSChatReceiver.java
@@ -0,0 +1,69 @@
+package bsp;
+
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+public class JMSChatReceiver {
+
+  private static String user = ActiveMQConnection.DEFAULT_USER;
+  private static String password = ActiveMQConnection.DEFAULT_PASSWORD;
+  private static String url = "tcp://192.168.0.18:61616";
+  private static String subject = "VSDBChat1";
+	
+  public static void main( String[] args ) {
+		
+	  // Create the connection.
+	  Session session = null;
+	  Connection connection = null;
+	  MessageConsumer consumer = null;
+	  Destination destination = null;
+			
+	  try {
+	    	
+			ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(user, password, url);
+			connection = connectionFactory.createConnection();
+			connection.start();
+		
+			// Create the session
+			session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+			destination = session.createTopic( subject );
+				  
+			// Create the consumer
+			consumer = session.createConsumer( destination );
+				
+			// Start receiving
+		  while (true) {
+			  TextMessage message = (TextMessage) consumer.receive();
+
+			  if (message != null) {
+				  System.out.println("Message received: " + message.getText());
+				  message.acknowledge();
+
+			  }
+		  }
+      //connection.stop();
+			
+	  } catch (Exception e) {
+	  	
+	    System.out.println("[MessageConsumer] Caught: " + e);
+	    e.printStackTrace();
+	      
+	  } finally {
+	  	
+			try { consumer.close(); } catch ( Exception e ) {}
+			try { session.close(); } catch ( Exception e ) {}
+			try { connection.close(); } catch ( Exception e ) {}
+			
+	  }
+
+  } // end main
+      
+}
+	
