8906e5de3c6ff8788ab7a8b6c9aa9c85ad0104df
tgm-patrick-koc <cash1v@hotmail.com>
2014-11-22 21:22:55 +0100

    Reciver updated

--- src/Communication/Reciver.java
+++ src/Communication/Reciver.java
@@ -39,24 +39,6 @@ public class Reciver implements Runnable {
 		}
 	}
 
-	public Reciver(String user, String password, String url) {
-		this.user=user;
-		this.password=password;
-		this.url=url;
-
-		connectionFactory = new ActiveMQConnectionFactory(user, password, url);
-		try {
-			connection = connectionFactory.createConnection();
-			session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-			destination = session.createTopic( chatroom );
-			consumer = session.createConsumer( destination );
-
-		} catch (JMSException e) {
-
-		}
-
-	}
-
 	public void Reciver(String user, String password, String url, String chatroom) {
 		this.user=user;
 		this.password=password;
